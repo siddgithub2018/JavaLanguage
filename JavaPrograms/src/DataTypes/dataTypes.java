fd60b51df4878152f5c12b9a70281446da551a41
SIDD <SIDD@SIDDHARTH-QA>
2021-03-08 21:52:22 +0530

    Committed

---
+++ JavaPrograms/src/DataTypes/dataTypes.java
@@ -0,0 +1,30 @@
+package DataTypes;
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+	
+		boolean flag = true;
+		byte range;
+	    range = 124;
+	    short temperature;
+	    int rangeint = -4250000;
+	    temperature = -200;
+	    long rangelong = -42332200000L;
+	    double numberdouble = -42.3;
+	    float number = -42.3f;
+	    char letter = '\u0051';
+	    String myString = "Java Programming";
+	    System.out.println(rangeint); 
+		System.out.println(flag);
+	    System.out.println(range);
+	    System.out.println(temperature);
+	    System.out.println(rangelong);
+	    System.out.println(numberdouble);
+	    System.out.println(number);
+	    System.out.println(letter);
+	    System.out.println(myString);
+	    
+	}
+
+}
---
+++ JavaPrograms/src/Operators/oPeratorsAssignment.java
@@ -0,0 +1,24 @@
+package Operators;
+
+public class oPeratorsAssignment {
+
+	public static void main(String[] args) {
+		
+		// create variables
+	    int a = 4;
+	    int var;
+
+	    // assign value using =
+	    var = a;
+	    System.out.println("var using =: " + var);
+
+	    // assign value using =+
+	    var += a;
+	    System.out.println("var using +=: " + var);
+
+	    // assign value using =*
+	    var *= a;
+	    System.out.println("var using *=: " + var);
+	}
+
+}
