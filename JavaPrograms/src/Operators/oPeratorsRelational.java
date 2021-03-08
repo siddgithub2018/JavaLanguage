a4c042471e1f9806cd793782992633d6b2437d1d
SIDD <SIDD@SIDDHARTH-QA>
2021-03-08 22:52:00 +0530

    Added Logical Class : operatorLogical

--- JavaPrograms/src/Operators/oPeratorsRelational.java
+++ JavaPrograms/src/Operators/oPeratorsRelational.java
@@ -4,11 +4,11 @@ public class oPeratorsRelational {
 
 	public static void main(String[] args) {
 		//create variables//
-		int a = 7;
+		int a = 11;
 		int b = 11;
 		
 		 // value of a and b
-		System.out.println("a is :" + a + "b is : " +b);
+		System.out.println("a is : " + a + "and b is : " + b);
 		
 		 // == operator
 	    System.out.println(a == b);  // false
