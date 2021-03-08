3622f3a601d4b9d031ba7ca79294e41573dccdf7
SIDD <SIDD@SIDDHARTH-QA>
2021-03-08 22:13:03 +0530

    Added Relational Operator Java Class

---
+++ JavaPrograms/src/Operators/oPeratorsRelational.java
@@ -0,0 +1,33 @@
+package Operators;
+
+public class oPeratorsRelational {
+
+	public static void main(String[] args) {
+		//create variables//
+		int a = 7;
+		int b = 11;
+		
+		 // value of a and b
+		System.out.println("a is :" + a + "b is : " +b);
+		
+		 // == operator
+	    System.out.println(a == b);  // false
+
+	    // != operator
+	    System.out.println(a != b);  // true
+
+	    // > operator
+	    System.out.println(a > b);  // false
+
+	    // < operator
+	    System.out.println(a < b);  // true
+
+	    // >= operator
+	    System.out.println(a >= b);  // false
+
+	    // <= operator
+	    System.out.println(a <= b);  // true	 
+
+	}
+
+}
